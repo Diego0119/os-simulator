57ee7fb46a9d2ec1e9fa311bad1daf8af6328159
Diego0119 <diegosanhuezaaguilar@gmail.com>
2024-10-03 18:28:22 -0300

    bloques de memoria

--- incs/memoria.h
+++ incs/memoria.h
@@ -1,11 +1,24 @@
+#define TAMANO_MEMORIA 1024 // en kb
 
-
+// sera para representar la lista de procesos en memoria
 // los bloques de memoria sera una lista enlazada, aun no se si simple o doble
 struct bloque_memoria
 {
-    int tamaño;
-    int libre;
-    BloqueMemoria *bloque_siguiente;
+    int tamaño;           // tamaño del bloque kb
+    int libre;            // 1 libre, 0 ocupado
+    int proceso_asignado; // puede ser -1 s no tiene proceso
 };
 
-typedef struct bloque_memoria BloqueMemoria;
+typedef struct bloque_memoria BloqueMemoria; // tamaño fijo de memoria del sistema op
+
+BloqueMemoria Memoria[TAMANO_MEMORIA]; // memoria total
+
+struct bloque_asignado
+{
+    int inicio; // indice
+    int tamaño; // porte en kb del espacio asignado
+    BloqueAsignado *siguiente;
+    BloqueAsignado *atras;
+};
+
+typedef struct bloque_asignado BloqueAsignado; // bloque asignado a un proceso
--- incs/proceso.h
+++ incs/proceso.h
@@ -5,7 +5,7 @@ struct proceso
     char nombre[10];
     int tiempo_ejecucion;
     int memoria_solicitada;
-    int estado;
+    int estado; // 0 nuevo, 1 listo, 2 ejecutando, 3 bloqueado, 4 finalizado
 };
 
 typedef struct proceso Proceso;
