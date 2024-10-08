53c858c05d356add8b7ff82a0c6d36f3ed1ec892
Diego0119 <diegosanhuezaaguilar@gmail.com>
2024-10-08 15:05:15 -0300

    save

--- incs/planificador.h
+++ incs/planificador.h
@@ -1,15 +1,15 @@
-#include "proceso.h"
+#include "./header.h"
 // aca esta el planificador de procesos, el so planificara con FIFO inicialmente, el primer proceso que llega sera el primero
 // que ejecute
 // aca en el planificador debo implementar FIFO
 
 // esto es una lista doblemente enlazada de un proceso
-typedef struct bloque_asignado BloqueAsignado;
+
 struct bloque_asignado
 {
     Proceso proceso;
-    BloqueAsignado *siguiente;
-    BloqueAsignado *atras;
+    Nodo *next;
+    Nodo *prev;
 };
 
 // bloque asignado a un proceso
@@ -17,6 +17,20 @@ typedef struct bloque_asignado Lista; // sera lo que se pasara a las funciones q
 
 struct cola
 {
-    BloqueAsignado *front;
-    BloqueAsignado *rear;
+
+    Nodo *front;
+    Nodo *rear;
 };
+
+void insertar(Proceso proceso, Lista lista)
+{
+    Nodo *nuevo_nodo = malloc(sizeof(Nodo));
+    nuevo_nodo->proceso = proceso;
+    nuevo_nodo->next = NULL;
+
+    if (esta_vacia(lista))
+    {
+    }
+}
+
+// void planificacion_del_sistema_operativo(Lista lista);
--- src/planificador.c
+++ src/planificador.c
@@ -1,2 +1,8 @@
 // aca se deberia implementar el planificador del so, se podria implementar primero
 // FIFO (el primer proceso en entrar es el primero en ser ejecutado)
+#include "../incs/header.h";
+
+void planificacion_del_sistema_operativo(Lista lista)
+{
+    // aca la cola debe manejar los procesos
+}
